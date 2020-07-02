a0060d8034f801b0fee26963cabd2c839b1b281f
soilee <hande.sof@gmail.com>
2020-07-02 22:55:53 +0900

    ex07

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | sed -e '1,10d' | awk 'NR %2==0 ' |  sort -r | sed -n '$FT_LINE1,$FT_LINE2p'
+cat /etc/passwd | sed -e '1,10d' | awk 'NR %2==0 ' |  sort -r | sed -n '/$FT_LINE1/,/$FT_LINE2/p'
